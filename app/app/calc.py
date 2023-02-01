0e0e9cfa41bb41e91766a849ec02e5f6d28fff53
cristianvitor01 <cristianvitoralves@gmail.com>
2023-02-01 14:51:38 -0300

    fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,11 @@ Calculator functions
 """
 
 
-
 def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return results"""
-    return y - x 
+    return y - x
