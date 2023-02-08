faafb73c109d11155238f8583b404670757d699f
cristianvitor01 <cristianvitoralves@gmail.com>
2023-02-08 10:14:00 -0300

    implemented user api

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,7 @@ from drf_spectacular.views import (
     SpectacularAPIView, SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
