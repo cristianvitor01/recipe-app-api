6b1180d3f51f368bdf5ffba84b577d0f3719fe9a
cristianvitor01 <cristianvitoralves@gmail.com>
2023-02-14 14:55:58 -0300

    implemented update ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,7 +54,9 @@ class TagViewSet(mixins.UpdateModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientsViewSet(mixins.UpdateModelMixin,
+                         mixins.ListModelMixin,
+                         viewsets.GenericViewSet):
     """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
