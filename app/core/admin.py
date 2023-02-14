0263d9ebfa74ebc15c8740986970dc6ad633083b
cristianvitor01 <cristianvitoralves@gmail.com>
2023-02-14 10:34:03 -0300

    add Ingredient model and tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -47,3 +47,4 @@ class UserAdmin(BasedUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
